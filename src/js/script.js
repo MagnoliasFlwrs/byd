a9abdac44d39fc79cdde661b942bedc2d72e77ac
martasavenok <magnolia.sav@gmail.com>
2024-11-06 00:03:16 +0300

    update project

--- src/js/script.js
+++ src/js/script.js
@@ -19,12 +19,67 @@ if (heroSwiper) {
     const swiper = new Swiper(heroSwiper, {
         slidesPerView: 1,
         loop: true,
-        autoplay: {
-            delay: 2500,
-        },
+        // autoplay: {
+        //     delay: 2500,
+        // },
         pagination: {
             el: '.hero-swiper-pagination',
             clickable: true,
         },
     });
 }
+
+const modelsSwiper = document.querySelector('.models-swiper');
+
+if (modelsSwiper) {
+    const swiper = new Swiper(modelsSwiper, {
+        slidesPerView: 1,
+        loop: true,
+        spaceBetween: 30,
+        autoplay: {
+            delay: 2500,
+        },
+
+        breakpoints: {
+            600:{
+                slidesPerView:1.2
+            },
+            800: {
+                slidesPerView: 1.5,
+            },
+            1200: {
+                slidesPerView:2.2
+            },
+            1550: {
+                slidesPerView:3
+            }
+        },
+        navigation: {
+            nextEl: '.models-swiper-button-next',
+            prevEl: '.models-swiper-button-prev',
+        }
+    });
+}
+
+let lastScrollTop = 0;
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', function() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > 5) {
+        if (scrollTop > lastScrollTop) {
+
+            header.classList.add('scrollable');
+        } else {
+
+            header.classList.remove('scrollable');
+        }
+    }
+    // else {
+    //
+    //     header.style.transform = 'translateY(0)';
+    // }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+});
